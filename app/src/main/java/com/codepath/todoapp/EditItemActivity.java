a0d39731bb5f897032a8900cb1334238d5a396b0
RodneyDouyon <42134781+RodneyDouyon@users.noreply.github.com>
2018-09-29 09:24:26 -0400

    Initial commit

---
+++ app/src/main/java/com/codepath/todoapp/EditItemActivity.java
@@ -0,0 +1,27 @@
+package com.codepath.todoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class EditItemActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_item);
+
+        String text = getIntent().getStringExtra("itemName");
+        int position = getIntent().getIntExtra("position", 0);
+
+    }
+
+
+}
